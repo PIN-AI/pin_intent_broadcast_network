12b2d5f951177063d7ae69ad9deebf01b9c56b6c
PisK4 <pis@pinai.io>
2025-07-29 15:39:05 +0800

    commit

---
+++ cmd/pin_intent_broadcast_network/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"pin_intent_broadcast_network/internal/biz"
+	"pin_intent_broadcast_network/internal/conf"
+	"pin_intent_broadcast_network/internal/data"
+	"pin_intent_broadcast_network/internal/server"
+	"pin_intent_broadcast_network/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
